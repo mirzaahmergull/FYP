9ba867cf9d794a50b41d6f369c1178b65120434d
Nouman Mustafa <noumanmustafa@hotmail.com>
2022-06-09 13:20:48 +0500

    add models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+#Basic
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(max_length=150)
+    password= models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def update_profile_signal(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
+    
+#Business
+class BusinessProfile(models.Model):
+    vendor_type = models.CharField(max_length=100, blank=True) 
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    business_name = models.CharField(max_length=100, blank=True)
+    full_name = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(max_length=150)
+    password= models.CharField(max_length=45)
+    
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def update_Businessprofile_signal(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
