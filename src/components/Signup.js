5397bed2d8b2bcd4aa09937860bcbe0087e74bc9
Nouman Mustafa <noumanmustafa@hotmail.com>
2022-06-10 23:34:26 +0500

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -4,50 +4,49 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { Link } from "react-router-dom";
 import React from "react";
-import axios from "axios";
+import axios from 'axios';
 import backendProfileURL from "./BackendInfo";
 
-const  SignUp = () => {
-    
+const  SignUp =() => {
+    let formdata={}  
     const [formValue, setFormValue] = React.useState(
-      {"full_name":'', 'password':'', 'email':'' }
+      {"full_name":null, 'password':null, 'email':null }
     )
-  const  handleSubmit = (event) => {
-    console.log("IN FORM", event)
-    const SignUpFormData = new FormData();
+    let SignUpFormData = new FormData();
   
-  SignUpFormData.append("full_name", formValue.email)
+  const  handleSubmit = async (event) => {
+    console.log("IN FORM", formValue)
+  
+  SignUpFormData.append("full_name", formValue.full_name) 
+  SignUpFormData.append("email", formValue.email)
   SignUpFormData.append("password", formValue.password)
   
-  try {
-    // make axios post request
-    console.log("DEBUG","FILE(Signup.js)","LINE(24)",SignUpFormData,"requesturl(",backendProfileURL,")");
-    // const response = axios.get({
-    //   method: "post",
-    //   url: 'http://127.0.0.1:8000/api/profile/',
-    //   data: SignUpFormData,
-    //   headers: { "Content-Type": "multipart/form-data" },
-    // });
-    const response = axios({
-      method: "post",
-      url: 'http://127.0.0.1:8000/api/profile/',
-      // url: '^api/profile/$',
-      // url: "http://127.0.0.1:8000/api/profile/?format=json",
-      data: SignUpFormData,
-      headers: {
-         "Content-Type": "multipart/form-data"
-         },
-      // headers: {
-      //   'Content-type': 'application/json'
-      // }
-    });
-  } catch(error) {
-    console.log("DEBUG","FILE(Signup.js)","LINE(40)",error.repsonse)
-  }
+  console.log("FORM VAR(formdata)",formdata,"FORM VAR(SignUpFormData)",SignUpFormData)
+  // try {
+    const resp = await axios(
+      {
+        method:"post",
+        url: 'http://127.0.0.1:8000/api/profile/',
+        data:SignUpFormData,
+        headers: { "Content-Type": "application/json" }
+      }
+     ).then(resp=>{
+       console.log("success")
+       console.table(resp)
+     }).catch(err=>{
+       console.log("DEBUG")
+       console.log(err)
+     })
+     ;
+    console.log(resp);
+
   }
 
+  
+  
   const handleChange = (event) => {
-   console.log(event.target.value)
+
+    console.log("DEBUG event",event)
     setFormValue({
       ...formValue,
       [event.target.name]: event.target.value
@@ -75,7 +74,7 @@ const  SignUp = () => {
         <></> &nbsp;
       
         <Form className="justify-content-md-center" onSubmit={handleSubmit(handleChange)}>
-    
+        {/* <Form className="justify-content-md-center" onSubmit={handleSubmit}> */}
           <Form.Label style={{ fontSize: "50px" }}>Signup</Form.Label>
           <Form.Group
             className="mb-3 justify-content-md-center"
@@ -86,7 +85,7 @@ const  SignUp = () => {
               style={{ width: "350px", height: "100%" }}
               type="name"
               placeholder="Name"
-              // onChange = {handleChange}        
+              //  onChange = {(e)=>handleChange({...formValue,full_name:e.target.value})}        
             />
       
           </Form.Group>
@@ -94,34 +93,35 @@ const  SignUp = () => {
             <Form.Control
               style={{ width: "350px" }}
               placeholder="Enter email"
-              type="email"
-              // onChange = {handleChange}
-              
+              type="email"     
+              // onChange = {(e)=>handleChange({...formValue,email:e.target.value})}         
             />
+
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Control
               style={{ width: "350px" }}
               type="password"
               placeholder="Enter Password"
-              name="password"
-         
-            />
+              name="password" 
+             />
+            
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Control
               style={{ width: "350px" }}
               type="password"
               placeholder="Confirm Password"
-              name="password"              
-              
+              name="password"                
             />
+            
           </Form.Group>
           <Link to="/">
             <Button style={{ width: "250px" }} variant="primary" type="submit"
             >
               Submit
             </Button>
+            
           </Link>
           <></> &nbsp;
           <></> &nbsp;
@@ -129,6 +129,7 @@ const  SignUp = () => {
           <></> &nbsp;
           <></> &nbsp;
           <Link to="/SignupVendor">
+            
             <Button style={{ width: "250px" }} variant="primary" type="submit">
               Signup as Vendor
             </Button>
@@ -138,6 +139,7 @@ const  SignUp = () => {
           <p>
             Already a member? <Link to="/Login">Login</Link>
           </p>
+          
         </Form>
       </div>
       <></> &nbsp;<></> &nbsp;
