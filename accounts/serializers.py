6f6385c811bfa3edcb27e889e19be71666eb3b51
Nouman Mustafa <noumanmustafa@hotmail.com>
2022-06-13 23:22:46 +0500

    Added VendorImageSerializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Profile, BusinessProfile
+from .models import Profile, BusinessProfile, VendorImage
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model= Profile
@@ -10,3 +10,13 @@ class BusinessProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model= BusinessProfile
         fields="__all__"        
+
+        
+class VendorImageSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(required=False)
+    # print("SERAILIZER")
+
+    class Meta:
+        model = VendorImage
+        fields = ('image_key', 'business_name', 'vendor_type',
+                  'title', 'image', 'creation_date')
